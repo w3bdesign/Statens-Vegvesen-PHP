b461b29624571acea0923cd30f3bda4555e2d65e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-18 23:23:17 +0200

    Autoloader

---
+++ autoloader.php
@@ -0,0 +1,15 @@
+<?php
+spl_autoload_register(function ($class) {
+    // Define the base directory for the namespace prefix
+    $baseDir = __DIR__ . '/classes/';
+
+    // Replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators, and add ".php" to the file name
+    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+
+    // If the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+?>
