26c11868afd5fba4d1ff434182412e4672f4d437
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-14 00:59:58 +0200

    Reduce complexity

--- classes/Application.php
+++ classes/Application.php
@@ -11,6 +11,8 @@ use Twig\Loader\FilesystemLoader;
 use Exception;
 use Dotenv;
 
+
+
 class Application
 {
     private $apikey;
@@ -18,20 +20,15 @@ class Application
 
     public function __construct()
     {
-        // Load environment variables
         $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
         $dotenv->load();
 
-
-        // Set up Twig
         $loader = new FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/views');
         $this->twig = new Environment($loader);
 
-        // Set API key
         $this->apikey = isset($_ENV["STATENS_VEGVESEN_API_KEY"]) ? $_ENV["STATENS_VEGVESEN_API_KEY"] : null;
 
         if (!$this->apikey) {
-            // Throw an exception if API key is missing
             throw new Exception('API nøkkel er ikke satt i .env');
         }
     }
@@ -40,34 +37,40 @@ class Application
     {
         session_start();
 
-        $hasError = false;
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["bilinformasjon"])) {
-            try {
-                $vehicleDataFetcher = new VehicleDataFetcher($this->apikey);
-                $regNummer = $_POST["bilinformasjon"];
-                $vehicleData = $vehicleDataFetcher->getVehicleData($regNummer);
-            } catch (Exception $e) {
-                $hasError = true;
-            }
-        }
+        $vehicleDataRendered = $this->handlePostRequest();
 
         echo $this->twig->render('main.html.twig');
 
-        if ($hasError) {
-            echo "<div class='container mt-5 text-center'>
-                <div class='alert alert-danger' role='alert'>
-                 " . $e->getMessage() . "
-                </div>
-                </div>";
-            return;
-        }
-
-        if (isset($vehicleData)) {
-            $vehicleDataRender = new VehicleDataRender($vehicleData);
-            echo $vehicleDataRender->render();
+        if (!empty($vehicleDataRendered)) {
+            echo $vehicleDataRendered;
         }
 
         echo $this->twig->render('footer.html.twig');
+    }
+
+    private function handlePostRequest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] !== "POST" || !isset($_POST["bilinformasjon"])) {
+            return '';
+        }
+
+        $vehicleDataRendered = '';
+
+        try {
+            $vehicleDataFetcher = new VehicleDataFetcher($this->apikey);
+            $regNummer = $_POST["bilinformasjon"];
+            $vehicleData = $vehicleDataFetcher->getVehicleData($regNummer);
+
+            $vehicleDataRender = new VehicleDataRender($vehicleData);
+            $vehicleDataRendered = $vehicleDataRender->render();
+        } catch (Exception $e) {
+            $vehicleDataRendered = "<div class='container mt-5 text-center'>
+                <div class='alert alert-danger' role='alert'>
+                 " . $e->getMessage() . "
+                </div>
+              </div>";
+        }
+
+        return $vehicleDataRendered;
     }
 }
